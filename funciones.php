96c2e7fb2f2a421c452b6f3a9eab480f8f5c2b46
Miguel <miguelalbencamunoz.guadalupe@alumnado.fundacionloyola.net>
2025-09-30 11:35:06 +0200

    Reorder and comment utility functions
    Moved the 'miFuncion' function below 'intereses' and added comments to clarify their intended use (Select and Checkbox). This improves code organization and readability.

--- funciones.php
+++ funciones.php
@@ -1,18 +1,19 @@
 <?php
-function miFuncion() {
-    return [
-        1 => ["Bdj" => "Badajoz"],
-        2 => ["Mrd" => "Mérida"],
-        3 => ["Ccs" => "Cáceres"]
-    ];
-}
-
+//Select
 function intereses() {
     return [
         "Energías Renovables",
         "Sostenibilidad y Vida Eco-friendly",
         "Impacto del Calentamiento Global",
         "Políticas Ambientales"
+    ];
+}
+//Checkbox
+function miFuncion() {
+    return [
+        1 => ["Bdj" => "Badajoz"],
+        2 => ["Mrd" => "Mérida"],
+        3 => ["Ccs" => "Cáceres"]
     ];
 }
 ?>
